7e4588d9ab288b6b37dc9e7b1c2eafd981baa2d8
Kuan Jia Jun <jiajun2838@gmail.com>
2024-04-23 00:05:40 +0800

    Scripts Updated
    -Player controller
    -Spawn manager
    scripts added

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    private float movingSpeed = 20.0f; 
+    private float xRangeLeft = -2.6f;
+    private float xRangeRight = 1.7f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerHorizontalInput();
+    }
+
+    //Method to control user to Left or Right by pressing Left/Right key
+    void PlayerHorizontalInput()
+    {
+         if (transform.position.z < xRangeLeft)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, xRangeLeft);
+        }
+
+        if (transform.position.z > xRangeRight)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, xRangeRight);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * movingSpeed * horizontalInput);
     }
 }
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float spawnRangeZLeft = -3.0f;
+    private float spawnRangeZRight = 2.8f;
+    private float spawnPositionY = 4.0f;
+    private float spawnInterval = 2.0f;
+    private float startDelay = 1.0f;
+
+    public GameObject[] objectPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObject", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -15,4 +23,13 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+
+    void SpawnObject()
+    {
+        int objectIndex = Random.Range(0, objectPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(0, spawnPositionY, Random.Range(spawnRangeZLeft, spawnRangeZRight));
+
+        Instantiate(objectPrefabs[objectIndex], spawnPosition, objectPrefabs[objectIndex].transform.rotation);
+    }
+
 }
